1ed34b360a116ff6bd7dd69281023d003e001df2
Paolo Santori <code@paolosantori.it>
2018-08-31 13:04:06 +0000

    Updated karma

--- karma.conf.js
+++ karma.conf.js
@@ -42,7 +42,7 @@ module.exports = function(config) {
     customLaunchers: {
       ChromeHeadless: {
         base: 'Chrome',
-        flags: ['--headless', '--disable-gpu', '--remote-debugging-port=9222', 'http://0.0.0.0:9876/']
+        flags: ['--headless', '--disable-gpu', '--no-sandbox', '--remote-debugging-port=9222', 'http://0.0.0.0:9876/']
       }
     },
     singleRun: true,
